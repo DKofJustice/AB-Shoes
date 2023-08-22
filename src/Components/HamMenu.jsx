35da347e2481873142ba7d635fc6da3133f80ddd
Alex Benta <alexbenta20@gmail.com>
2023-08-22 14:40:07 +0300

    Partially completed the Mobile Menu

---
+++ src/Components/HamMenu.jsx
@@ -0,0 +1,10 @@
+
+export default function HamMenu() {
+  return (
+    <div className="bg-white fixed h-full w-[20rem]">
+        <div>
+
+        </div>
+    </div>
+  )
+}
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,7 @@
 import ABLogo from './../assets/AB Shoes Logo.svg'
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Header() {
   return (
@@ -11,21 +12,25 @@ export default function Header() {
             </div>
         </div>
 
-        <div className='w-full bg-white flex flex-row justify-center items-center px-32 py-8'>
+        <div className='w-full bg-white flex flex-row justify-start md:justify-center items-center px-10 lg:px-32 py-8'>
             <div className='w-fit'>
-                <img src={ABLogo} alt="Site Logo" className='cursor-pointer' />
+                <img src={ABLogo} alt="Site Logo" className='cursor-pointer h-6 md:h-full' />
             </div>
 
-            <ul className='w-full flex flex-row justify-center items-center gap-x-[3rem] font-semibold'>
+            <ul className='hidden md:block w-full md:flex flex-row justify-center items-center gap-x-[3rem] font-semibold'>
                 <li className='cursor-pointer'>MEN</li>
                 <li className='cursor-pointer'>WOMEN</li>
                 <li className='cursor-pointer'>NEW ARRIVALS</li>
             </ul>
 
-            <ul className='w-fit flex flex-row justify-end items-center gap-x-[3rem]'>
+            <ul className='hidden w-fit md:flex flex-row justify-end items-center gap-x-[3rem]'>
                 <li className='cursor-pointer'><PersonOutlineIcon /></li>
                 <li className='cursor-pointer'><ShoppingCartIcon /></li>
             </ul>
+
+            <div className='text-black ml-auto cursor-pointer md:hidden'>
+                <MenuIcon />
+            </div>
         </div>
     </header>
   )
